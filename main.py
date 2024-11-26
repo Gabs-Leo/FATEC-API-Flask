10a3f0723ab61817fa20e5af698ccffbd33cbdfe
Gabriel Taveira <gabrieldemoraistaveira@gmail.com>
2024-11-26 14:43:02 -0300

    added bootstrap layout for better ui

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from flask import Flask
 import database
 from controllers.discipline import discipline_bp
 from controllers.course import course_bp
+from controllers.home import home_bp
 from controllers.teacher import teacher_bp
 from controllers.student import student_bp
 
 app = Flask(__name__)
 database.init()
 
+app.register_blueprint(home_bp)
 app.register_blueprint(discipline_bp)
 app.register_blueprint(course_bp)
 app.register_blueprint(teacher_bp)
